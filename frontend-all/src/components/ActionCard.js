64ef5ac7b28caf428f4c08be024793bcdefad60f
Manoj-0202 <manojmanu3318@gmail.com>
2025-08-21 13:01:24 +0530

    updated styles

--- frontend-all/src/components/ActionCard.js
+++ frontend-all/src/components/ActionCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import '../styles/home.css';
 
 const ActionCard = ({ image, title }) => {
   return (
     <div className="action-card">
-      <img src={image} alt={title} />
-      <p>{title}</p>
+      <img src={image} alt={title} className="action-card-image" />
+      <p className="action-card-title">{title}</p>
     </div>
   );
 };
